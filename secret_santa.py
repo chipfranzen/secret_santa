c1e109bc5caceedfe69767e5c341e79db89c75eb
chipfranzen <chip.franzen@gmail.com>
2019-11-19 21:56:51 -0800

    Remove test message.

--- secret_santa.py
+++ secret_santa.py
@@ -66,7 +66,7 @@ def get_valid_draw(fam, couples):
 
 def get_message(santa, elf, santa_email, sender_address):
     msg = EmailMessage()
-    msg.set_content(f"TEST Hey {santa}, get a gift for {elf} this year! TEST")
+    msg.set_content(f"Hey {santa}, get a gift for {elf} this year!")
     msg["Subject"] = "TOP SECRET"
     msg["From"] = sender_address
     msg["To"] = santa_email
